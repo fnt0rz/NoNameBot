330fe4cf441eedb5fc160ac7a227ca05e2e2198e
fnt <>
2021-04-06 15:47:00 +0000

    remove old on join event

--- main.py
+++ main.py
@@ -16,12 +16,6 @@ async def on_ready():
   return await client.change_presence(activity=discord.Activity(type=1, name='World of Warcraft'))
 
 @client.event
-async def on_member_join(member):
-  print('{0} joined the server'.format(member.name))
-  channel = client.get_channel(820241232222748692)
-  await channel.send('Hello {0}!'.format(member.name))
-
-@client.event
 async def on_message(message):
   await checkCommandFor(message, client)
 
