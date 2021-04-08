58b43ca4acd99a4ac8a0a7a428961e868aa51a25
fnt <>
2021-04-08 09:24:54 +0000

    added support for multiple encounters

--- Raiddata/raidinfo.py
+++ Raiddata/raidinfo.py
@@ -14,19 +14,21 @@ def getRaidData():
 
   return raids
 
-def getNextRaid():
+def getNextRaids(numberOfRaids):
 
   raids = getRaidData()
-  found = False
+  found = 0
   d = datetime.date.today()
+  foundRaids = []
 
-  while found == False:
+  while found < numberOfRaids:
     for raid in raids:
       if d.strftime("%Y-%m-%d") == raid["date"]:
-        found = True
-        return raid
-        break
+        foundRaids.append(raid)        
+        found =+ 1
     d = d + datetime.timedelta(days=1)
+  
+  return foundRaids
   
 def getRoster():
   raidData = getNextRaidInfo()
@@ -34,9 +36,11 @@ def getRoster():
 
   return roster
 
+def getNextRaid():
+  return getNextRaids(1)
 
 def getNextRaidInfo():
-  raidId = getNextRaid()["id"]
+  raidId = getNextRaid()[0]["id"]
   raidurl = "https://wowaudit.com/v1/raids/{0}".format(str(raidId))
   rstr = requests.get(raidurl, headers={'Authorization':key}).json()
 
@@ -50,26 +54,35 @@ def getRaiders():
 
 def parseRaidRoster(raidData, raiders):
   lst = []
-  for r in raidData["signups"]:
-    if r["selected"] == True:
-      lst.append(r)
+  for boss in raidData["encounters"]:
+    if boss["enabled"] == True:
+      rlist = []
+      for r in boss["selections"]:
+        if r["selected"] == True:
+          rname = findRaiderName(r["character_id"], raiders)
+          rlist.append({"name": rname, "role": r["role"]})
+      lst.append({"boss": boss["name"], "raiders": rlist})
   return lst
+
+def findRaiderName(id, raiders):
+  for r in raiders:
+    if id == r["id"]:
+      return r["name"]
 
 def createRoster(raidlist):
   melee = []
   ranged = []
   healers = []
   tanks = []
-
   for c in raidlist:
-    if c["role"] == "Ranged":
-      ranged.append(c["character"]["name"])
-    if c["role"] == "Melee":
-      melee.append(c["character"]["name"])
-    if c["role"] == "Tank":
-      tanks.append(c["character"]["name"])
-    if c["role"] == "Heal":
-      healers.append(c["character"]["name"])
+    if c["raider"]["role"] == "Ranged":
+      ranged.append(f"{c['raider']['name']} ({ c['selected'] })")
+    if c["raider"]["role"] == "Melee":
+      melee.append(f"{c['raider']['name']} ({ c['selected'] })")
+    if c["raider"]["role"] == "Tank":
+      tanks.append(f"{c['raider']['name']} ({ c['selected'] })")
+    if c["raider"]["role"] == "Heal":
+      healers.append(f"{c['raider']['name']} ({ c['selected'] })")
 
   roster = {
     "melee": formatString(melee),
@@ -80,15 +93,35 @@ def createRoster(raidlist):
 
   return roster 
 
-def createEmbeddedMessage(raidinfo, raidData):
-  roster = createRoster(raidinfo)
-  footerNote = "Total sign-ups: {0} · brought to you by Chookity!".format(str(len(raidData["signups"])))
+def createRosterList(raidinfo):
+  lst = []
+  rst = []
 
+  for boss in raidinfo:
+    for r in boss["raiders"]:
+      lst.append(r)
+
+  for raider in lst:
+    n = lst.count(raider)
+    if {"raider": raider, "selected": n} not in rst:
+      rst.append({"raider": raider, "selected": n})
+
+  return rst
+
+def createEmbeddedMessage(raidinfo, raidData):
   raidUrl = "https://wowaudit.com/eu/tarren-mill/noname/main/raids/{0}".format( raidData["id"])
 
-  message= discord.Embed(description="Roster for _**{0}**_ raid on _**{1}**_".format(raidData["instance"], raidData["date"]), colour= discord.Color.green())
-  message.set_author(name="Raid Roster")
+  message= discord.Embed(description=f"Time: {raidData['start_time']} - {raidData['end_time']} **·** difficulty: {raidData['difficulty']}", colour= discord.Color.green())
+  message.set_author(name="Roster for {0} raid on {1}".format(raidData["instance"], raidData["date"]))
   message.add_field(name="\u200b", value="\u200b")
+
+  roster = createRoster(createRosterList(raidinfo))
+
+  if len(raidinfo) > 1:
+    message.add_field(name=f"Current roster for { len(raidinfo)} bosses:", value="\u200b", inline=False)
+  else: 
+    message.add_field(name=f"Current roster for { raidinfo[0]['boss']}:", value="\u200b", inline=False)
+
 
   if len(roster["tanks"]) > 0:
     message.add_field(name=":shield: Tanks", value=roster["tanks"], inline=False)
@@ -106,8 +139,12 @@ def createEmbeddedMessage(raidinfo, raidData):
     message.add_field(name=":crossed_swords: Melee", value=roster["melee"], inline=False)
   else:
     addNoRaidersField(message, ":crossed_swords: Melee")
-  message.add_field(name="\u200B", value=f"**[Roster]({raidUrl})**")
-  message.set_footer(text=footerNote)
+
+  if len(raidinfo) > 1:
+    message.add_field(name="\u200B", value="_** More then 1 bosses are selected be sure to check the roster following the link below **_", inline=False)
+
+  message.add_field(name="\u200B", value=f"**[Roster]({raidUrl})**", inline=False)
+  message.set_footer(text="brought to you by Chookity!")
   
   return message
 
@@ -116,7 +153,6 @@ def formatString(item):
 
 def addNoRaidersField(message, icon):
   message.add_field(name=icon, value="No players yet", inline=False)
-
 
 
 
