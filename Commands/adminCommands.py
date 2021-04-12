95b1bd30f955a38ee775d91b4f7e0b71b3d2126c
fnt <>
2021-04-12 06:59:34 +0000

    fixed rights

--- Commands/adminCommands.py
+++ Commands/adminCommands.py
@@ -6,23 +6,26 @@ class AdminCommands(commands.Cog):
     self.bot = bot
 
   @commands.command(hidden=True)
+  @commands.has_role("Bot Admin")
   async def setChannel(self, ctx):
     await channelbase.setGeneralChannel(ctx)
 
   @commands.command(hidden=True)
+  @commands.has_role("Bot Admin")
   async def getChannel(self, ctx):
     await channelbase.getGeneralChannel(ctx, self.bot)
 
   @commands.command(hidden=True)
+  @commands.has_role("Bot Admin")
   async def setDelay(self, ctx, delay: str):
     result = channelbase.setSocialTimer(delay)
     await ctx.send(result)
 
   @commands.command(hidden=True)
+  @commands.has_role("Bot Admin")
   async def getDelay(self, ctx):
     result = channelbase.getSocialTimerText()
     await ctx.send(result)
-
 
 def setup(bot):
   bot.add_cog(AdminCommands(bot))
--- Social/channelbase.py
+++ Social/channelbase.py
@@ -1,30 +1,23 @@
 from replit import db
-import discord
 
 async def setGeneralChannel(msg):
-  if checkRole(msg):
     c = msg.channel
     result = setGeneralChannelId(c.id)
     if result == True:
       return await msg.channel.send(f"Chookity-pah, { c.mention } is now set as general channel.")
     else: 
       return await msg.channel.send("Failed to set channel")
-  
-  return await msg.channel.send("Hmmmm.")
 
 async def getGeneralChannel(msg, bot):
-  if checkRole(msg):
-    cid = getGeneralChannelId()
-    if not cid:
-      return await msg.channel.send("Not channel set yet, try using $setChannel")
-    
-    c = bot.get_channel(cid)
-    if not c:
-      return await msg.channel.send("Not channel set yet, try using $setChannel")
-
-    return await msg.channel.send(f"Chookity-Chook, { c.mention } is currently set as general channel.")
+  cid = getGeneralChannelId()
+  if not cid:
+    return await msg.channel.send("Not channel set yet, try using $setChannel")
   
-  return await msg.channel.send("Hmmmm.")
+  c = bot.get_channel(cid)
+  if not c:
+    return await msg.channel.send("Not channel set yet, try using $setChannel")
+
+  return await msg.channel.send(f"Chookity-Chook, { c.mention } is currently set as general channel.")
 
 def getSocialTimerText():
   return f"Chookity-Chook, { getSocialTimer() } minutes is currently set as delay."
@@ -36,15 +29,9 @@ def setSocialTimer(minutes):
 def getSocialTimer():
   return db["socialTimer"]
 
-
-
 def setGeneralChannelId(id):
   db["generalChannel"] = id
   return True
 
 def getGeneralChannelId():
   return db["generalChannel"]
-
-def checkRole(msg):
-  role = discord.utils.find(lambda r: r.name == 'Bot Admin', msg.guild.roles)
-  return role in msg.author.roles
